d533a6ce22b06e8cf099b5d628b8046e609100cc
Yusa Islam <yusaislam48@gmail.com>
2025-05-08 14:51:49 +0600

    Added port  for deployment

--- app.py
+++ app.py
@@ -172,4 +172,5 @@ def get_itinerary():
 # MAIN
 # ----------------------
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
