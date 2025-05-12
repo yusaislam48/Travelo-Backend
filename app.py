d19cd98b06d0cd4814588f75c7bbc53c0922757b
Yusa Islam <yusaislam48@gmail.com>
2025-05-13 05:47:29 +0600

    Update app to run on port 8000 with debug mode enabled

--- app.py
+++ app.py
@@ -172,5 +172,5 @@ def get_itinerary():
 # MAIN
 # ----------------------
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+    port = int(os.environ.get('PORT', 8000))
+    app.run(host='0.0.0.0', port=port, debug=True, use_reloader=True)
